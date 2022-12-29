90c585c607b1caaa63bc7c1815b7df213e19d13e
maisasb <maisasb15@gmail.com>
2022-12-29 18:31:01 -0300

    fix: border token

--- src/replacements/border.js
+++ src/replacements/border.js
@@ -11,10 +11,10 @@ const replaceBorderRadius = (data) => {
 };
 
 const replaceBorderWidth = (data) => {
-  data = data.replaceAll(/--border-width-none/g, "--gl-border-radius-none");
-  data = data.replaceAll(/--border-width-thin/g, "--gl-border-radius-hairline");
-  data = data.replaceAll(/--border-width-medium/g, "--gl-border-radius-thin");
-  data = data.replaceAll(/--border-width-thick/g, "--gl-border-width-thick");
+  data = data.replaceAll(/--border-width-none/g, "--gl-border-width-none");
+  data = data.replaceAll(/--border-width-thin/g, "--gl-border-width-xs");
+  data = data.replaceAll(/--border-width-medium/g, "--gl-border-width-md");
+  data = data.replaceAll(/--border-width-thick/g, "--gl-border-width-lg");
 
   return data;
 };
