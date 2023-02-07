04d6857d7724db44c59c784a58dced8de2173c5b
maisasb <maisasb15@gmail.com>
2023-02-07 10:36:49 -0300

    fix: spacing replace

--- src/replacements/spacing.js
+++ src/replacements/spacing.js
@@ -1,16 +1,19 @@
 const replaceSpacing = (data) => {
-  data = data.replaceAll(/--spacing-base-xx-small/g, "--gl-spacing-01");
-  data = data.replaceAll(/--spacing-base-x-small/g, "--gl-spacing-02");
-  data = data.replaceAll(/--spacing-base-small/g, "--gl-spacing-03");
-  data = data.replaceAll(/--spacing-base/g, "--gl-spacing-04");
-  data = data.replaceAll(/--spacing-base-large/g, "--gl-spacing-06");
-  data = data.replaceAll(/--spacing-medium-small/g, "--gl-spacing-08");
-  data = data.replaceAll(/--spacing-medium/g, "--gl-spacing-10");
-  data = data.replaceAll(/--spacing-medium-large/g, "--gl-spacing-12");
-  data = data.replaceAll(/--spacing-wide-small/g, "--gl-spacing-16");
-  data = data.replaceAll(/--spacing-wide/g, "--gl-spacing-16");
-  data = data.replaceAll(/--spacing-wide-large/g, "--gl-spacing-20");
-  data = data.replaceAll(/--spacing-wide-x-large/g, "--gl-spacing-20");
+  data = data.replaceAll(
+    "var(--spacing-base-xx-small)",
+    "var(--gl-spacing-01)"
+  );
+  data = data.replaceAll("var(--spacing-base-x-small)", "var(--gl-spacing-02)");
+  data = data.replaceAll("var(--spacing-base-small)", "var(--gl-spacing-03)");
+  data = data.replaceAll("var(--spacing-base)", "var(--gl-spacing-04)");
+  data = data.replaceAll("var(--spacing-base-large)", "var(--gl-spacing-06)");
+  data = data.replaceAll("var(--spacing-medium-small)", "var(--gl-spacing-08)");
+  data = data.replaceAll("var(--spacing-medium)", "var(--gl-spacing-10)");
+  data = data.replaceAll("var(--spacing-medium-large)", "var(--gl-spacing-12)");
+  data = data.replaceAll("var(--spacing-wide-small)", "var(--gl-spacing-16)");
+  data = data.replaceAll("var(--spacing-wide)", "var(--gl-spacing-16)");
+  data = data.replaceAll("var(--spacing-wide-large)", "var(--gl-spacing-20)");
+  data = data.replaceAll("var(--spacing-wide-x-large)", "var(--gl-spacing-20)");
 
   // data = data.replaceAll(//g, "");
 
